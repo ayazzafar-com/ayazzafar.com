fee6801924e9e8d82b64f97b56baa44753d98126
Ayaz Zafar <ayaziqbalzafar@gmail.com>
2022-11-06 19:11:06 +0000

    Create mail.php

---
+++ ayazzafar.com/mail.php
@@ -0,0 +1,14 @@
+<?php
+//get data from form  
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+$to = "ayaziqbalzafar@gmail.com";
+$subject = "Mail From website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: noreply@ayazzafar.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+?>
